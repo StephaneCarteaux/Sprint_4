39fb31f811489e8dfda9cd58009eb90ae1aafebb
Stéphane Carteaux <stephanecarteaux@gmail.com>
2024-04-23 13:05:03 +0200

    setup basic layout and views

---
+++ futbol-app/resources/views/clasificacion.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+    <x-slot:title>
+        Custom Title
+    </x-slot>
+    <div>
+        Clasificación
+    </div>
+</x-layout>
---
+++ futbol-app/resources/views/partidos.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+    <x-slot:title>
+        Custom Title
+    </x-slot>
+    <div>
+        Partidos
+    </div>
+</x-layout>
