9e652c7a9f1f58c03858fd240e0581c1fe34932e
Stéphane Carteaux <stephanecarteaux@gmail.com>
2024-04-23 13:01:01 +0200

    Create migration and models

---
+++ futbol-app/database/migrations/2024_04_22_153139_create_games_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('team1_id')->constrained(
+                table: 'teams',
+                indexName: 'fk_team1_id'
+            );
+            $table->foreignId('team2_id')->constrained(
+                table: 'teams',
+                indexName: 'fk_team2_id'
+            );
+            $table->unsignedInteger('game_number');
+            $table->dateTime('date');
+            $table->unsignedInteger('score1');
+            $table->unsignedInteger('score2');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('game');
+    }
+};
