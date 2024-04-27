6e76979b90ef60863de912227cd7877c505bfdea
Stéphane Carteaux <stephanecarteaux@gmail.com>
2024-04-27 18:55:02 +0200

    Fix model::booted() and Layout condition to display $activeLeague

--- futbol-app/app/Models/League.php
+++ futbol-app/app/Models/League.php
@@ -21,10 +21,23 @@ class League extends Model
     // and sets the active field to 0 if the league is not active
     protected static function booted()
     {
+        // Listen to the saving event
+        // If saved model is set to active, set all other models to 0
         static::saving(function (League $league) {
-            echo 'saved';
             if ($league->active == 1) {
                 self::where('id', '!=', $league->id)->update(['active' => 0]);
+            }
+        });
+
+        // Listen to the deleting event
+        // If deleted model is set to active, set first model to 1
+        static::deleting(function (League $league) {
+            if ($league->active == 1) {
+                $firstLeague = self::where('id', '!=', $league->id)->first();
+                if($firstLeague) {
+                    self::where('id', '!=', $league->id)->first()->update(['active' => 1]);
+                }
+                
             }
         });
     }
