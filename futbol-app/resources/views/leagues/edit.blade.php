36721600eae72f67d0890325b441960fb597c780
Stéphane Carteaux <stephanecarteaux@gmail.com>
2024-04-28 19:36:15 +0200

    Update edit function

--- futbol-app/resources/views/leagues/edit.blade.php
+++ futbol-app/resources/views/leagues/edit.blade.php
@@ -1,29 +1,28 @@
 <x-layout>
     <x-slot:title>
-        Crear liga
+        Editar liga
     </x-slot>
     <div>
         <h2 class="flex flex-row flex-nowrap items-center mt-16 uppercase">
             <span class="flex-grow block border-t border-gray-700"></span>
             <span
                 class="flex-none block mx-4 px-4 py-2.5 text-xl rounded leading-none font-medium bg-gray-900 text-white">
-                Crear nueva liga
+                Editar liga
             </span>
             <span class="flex-grow block border-t border-gray-700"></span>
         </h2>
 
         <div
             class="relative overflow-x-auto shadow-md sm:rounded-lg  bg-gray-700 mt-16 sm:w-full md:w-1/2 lg:w-1/3 xl:w-[600px] mx-auto">
-            <form action="{{ route('leagues.store') }}" method="post" class="max-w-sm mx-auto mt-8 mb-8">
+            <form action="{{ route('leagues.update', $league->id) }} " method="post" class="max-w-sm mx-auto mt-8 mb-8">
                 @csrf
-                <input type="hidden" id="active" name="active" value="1">
-                <input type="hidden" id="started" name="started" value="0">
+                @method('PATCH')
 
                 <!-- Name -->
                 <div class="mb-5">
                     <label for="name" class="block mb-1 text-sm font-medium text-white">Name:</label>
                     <input type="text" id="name" name="name"
-                        class="text-sm rounded-lg block w-full p-2 bg-gray-900 text-white" value="<?php echo isset($_POST['name']) ? $_POST['name'] : ''; ?>">
+                        class="text-sm rounded-lg block w-full p-2 bg-gray-900 text-white" value="{{ $league->name }}">
                 </div>
 
                 <div class="flex justify-between mt-5">
