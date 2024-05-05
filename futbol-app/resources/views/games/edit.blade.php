e96806b2a678f6d85538aa24a292ada528a8a85b
Stéphane Carteaux <stephanecarteaux@gmail.com>
2024-05-06 00:06:32 +0200

    Tailwind mb-6 en create and edit forms

--- futbol-app/resources/views/games/edit.blade.php
+++ futbol-app/resources/views/games/edit.blade.php
@@ -16,7 +16,7 @@
         <x-errors />
 
         <div
-            class="relative overflow-x-auto shadow-md sm:rounded-lg border-2 border-gray-700 bg-gray-300 mt-16 sm:w-full md:w-1/2 lg:w-1/3 xl:w-[600px] mx-auto">
+            class="relative overflow-x-auto shadow-md sm:rounded-lg border-2 border-gray-700 bg-gray-300 mt-16 mb-6 sm:w-full md:w-1/2 lg:w-1/3 xl:w-[600px] mx-auto">
             <form action="{{ route('games.update', $game->id) }}" method="post" class="w-full max-w-sm mx-auto mt-8 mb-8">
                 @csrf
                 @method('PATCH')
--- futbol-app/resources/views/leagues/edit.blade.php
+++ futbol-app/resources/views/leagues/edit.blade.php
@@ -15,7 +15,7 @@
         <!-- Errors template -->
         <x-errors/>
 
-        <div class="relative overflow-x-auto shadow-md sm:rounded-lg border-2 border-gray-700 bg-gray-300 mt-16 sm:w-full md:w-1/2 lg:w-1/3 xl:w-[600px] mx-auto">
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg border-2 border-gray-700 bg-gray-300 mt-16 mb-6 sm:w-full md:w-1/2 lg:w-1/3 xl:w-[600px] mx-auto">
             <form action="{{ route('leagues.update', $league->id) }} " method="post" class="max-w-sm mx-auto mt-8 mb-8">
                 @csrf
                 @method('PATCH')
