ceb39545214cf6a3a8670d36285b438c887dea39
Stéphane Carteaux <stephanecarteaux@gmail.com>
2024-05-09 00:37:46 +0200

    Take out getTeamStats() from RankingController

--- futbol-app/app/Services/TeamService.php
+++ futbol-app/app/Services/TeamService.php
@@ -7,7 +7,7 @@ use App\Models\Team;
 
 class TeamService
 {
-    public function getTeamsForActiveLeague()
+    public function getTeamsForActiveLeague(): Team
     {
         $activeLeague = LeagueUtility::getActiveLeague();
         $teams = Team::where('league_id', $activeLeague->id)->get();
