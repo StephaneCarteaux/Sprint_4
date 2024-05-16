14cf7afa559f719cc442589305b8d7c358bf9a01
Stéphane Carteaux <stephanecarteaux@gmail.com>
2024-05-16 17:32:38 +0200

    Minor update

--- futbol-app/resources/views/errors/404.blade.php
+++ futbol-app/resources/views/errors/404.blade.php
@@ -13,14 +13,14 @@
         <div class="p-12">
             <div class="flex items-center justify-center text-6xl font-bold pb-6">4<x-404-logo />4</div>
             <p class="text-3xl font-bold">{{ __('The page you are looking for could not be found.') }}</p>
-            
+
             <!-- Buttons container -->
             <div class="flex justify-center mt-6 space-x-6">
-            <!-- Return button -->
-            <x-secondary-button>
-                <a href="{{ route('home.index') }}">{{ __('back') }}</a>
-            </x-secondary-button>
-        </div>
+                <!-- Return button -->
+                <x-secondary-button>
+                    <a href="{{ route('home.index') }}">{{ __('back') }}</a>
+                </x-secondary-button>
+            </div>
         </div>
     </div>
 </x-app-layout>
